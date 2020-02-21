09226434658b3636c7137792ccced4dbc6a7f12c
Timothy Stewart <timothy.stewart18@yahoo.com>
2020-02-20 19:17:38 -0600

    Initial Commit

---
+++ tipCal3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCal3
+//
+//  Created by Timothy Stewart on 2/20/20.
+//  Copyright © 2020 Timothy Stewart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
